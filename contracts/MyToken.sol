afbbe848ebf9144284c31944dbdf54fb63f12b27
Neha-shukla <nshukl23@gmail.com>
2020-07-07 10:39:30 +0530

    1. smoke test

---
+++ contracts/MyToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier:UNLICENSED
+pragma solidity >=0.4.21 <0.7.0;
+
+contract TokenSale{
+    //constructor
+    //set total number of tokens
+    //read total number of tokens
+    uint256 public totalSupply;
+
+    constructor() public{
+        totalSupply = 1000000;
+    }
+}
+
+
